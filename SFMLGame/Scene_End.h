630887fe63f6447bb84a315505f84d0de5d08841
BrandonCarls <bcarls@carthage.edu>
2025-05-21 21:46:12 -0500

    Recreated Scene_End

--- SFMLGame/Scene_End.h
+++ SFMLGame/Scene_End.h
@@ -6,21 +6,20 @@ using namespace std;
 class Scene_End : public Scene
 {
 public:
-	Scene_End(GameEngine* gameEngine);
+    Scene_End(GameEngine* gameEngine);
 protected:
-	void init();
-	void update();
-	void onEnd();
-	void sRender();
-	void sDoAction(const Action& action);
-	void sAnimation();
-	void loadMenuBackground();
+    void init();
+    void update();
+    void onEnd();
+    void sRender();
+    void sDoAction(const Action& action);
+    void sAnimation();
+    void loadMenuBackground();
 
-	vector<string> menuStrings;
-	vector<string> levelPaths;
-	sf::Text menuText;
-	sf::Text winText;
-	string title;
-	int selectedMenuIndex = 0;
+    vector<string> menuStrings;
+    vector<string> levelPaths;
+    sf::Text menuText;
+    sf::Text winText;
+    string title;
+    int selectedMenuIndex = 0;
 };
-
